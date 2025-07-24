6a6474043adbcb395fedfd9090f7aee0224258c0
neee333ko <1260644864@qq.com>
2025-07-24 20:54:06 +0800

    feat: change test and add README

---
+++ README.md
@@ -0,0 +1,2 @@
+# neee333ko/log
+`neee333ko/log`基于`zap`进行封装，除了zap的基本功能外，还封装了其他高级功能。
--- logger_test.go
+++ logger_test.go
@@ -1,6 +1,8 @@
 package log
 
 import (
+	"context"
+	"sync"
 	"testing"
 )
 
@@ -11,4 +13,44 @@ func Test_V(t *testing.T) {
 	opts := InitOptions(WithLevel("debug"))
 	logger2 := New(opts).V(1)
 	logger2.Info("Test V1")
+}
+
+func Test_WithValues(t *testing.T) {
+	logger := New(nil)
+	logger.Info("msg without values")
+
+	logger = logger.WithValues("musician", "kanye")
+	logger.Info("msg with values")
+}
+
+func Test_WithName(t *testing.T) {
+	logger := New(nil)
+	logger.Info("msg without name")
+
+	logger = logger.WithNamed("nekologger")
+	logger.Info("msg with name")
+}
+
+func Test_Context(t *testing.T) {
+	logger := New(nil).WithValues("username", "kanye")
+
+	gc := context.Background()
+	vc := logger.WithContext(gc)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func(ctx context.Context) {
+		defer wg.Done()
+
+		logger := FromContext(ctx)
+		logger.Info("msg with context")
+	}(vc)
+
+	vc2 := context.WithValue(gc, KeyRequestID, "123456")
+	logger2 := New(nil).L(vc2)
+
+	logger2.Info("msg with context from L")
+
+	wg.Wait()
 }
